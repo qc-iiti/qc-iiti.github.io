f054f93c114a07e1bab27a00176905c90f8d2bc2
Arham Aneeq <arhamaneeq18@gmail.com>
2025-07-17 00:41:27 +0530

    Refactor Footer component and styles for improved layout and accessibility

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,20 +3,36 @@ import React from "react";
 const Footer : React.FC = () => {
     return <div id="Footer">
         <div className="footerSection">
-            <h4>Quantum Computing @ IIT Indore</h4>
-            <p>Made with ❤️ by the QC@IITI Web Dev Team.</p>
+            <h4>© 2025 Quantum Computing Club, IIT Indore</h4>
+            <p>Made with ❤️ by the QC IITI Web Dev Team.</p>
             <p>See a bug? Raise an <a href="https://github.com/qc-iiti/qc-iiti.github.io/issues">Issue</a>!</p>
         </div>
         <div className="footerSection">
             <h4>
+                Contact Us
+            </h4>
+                <p>quantum.iiti.ac.in</p>
+            <h4>
                 Follow Us
             </h4>
-                <a href="http://github.com/qc-iiti">GitHub</a>
-                <a href="http://linkedin.com/company/quantumcomputingclub">LinkedIn</a>
-                <a href="http://instagram.com/">Instagram</a>
+                <p>
+                    <a href="http://github.com/qc-iiti">GitHub</a>
+                </p><p>
+                    <a href="http://linkedin.com/company/quantumcomputingclub">LinkedIn</a>
+                </p><p>
+                    <a href="http://instagram.com/">Instagram</a>
+                </p>
         </div>
         <div className="footerSection">
-            <p>© 2025 Quantum Computing Club, IIT Indore</p>
+            
+                
+                <h5>President</h5>
+                <p>Arham Aneeq</p>
+                <p>+91 xxxxxxxxxxxx / mems2400050009@iiti.ac.in</p>
+
+                <h5>Vice-Presidents</h5>
+                <p>Aarush Bindod (+91 xxxxxxxxxxxx / ep...)</p>
+                <p>V Hemal (+91 xxxxxxxxxxxx / ep...)</p>
         </div>
     </div>
 }
