0479842a866f55d9c0fa61ddc1fc30372c45e700
Arham Aneeq <arhamaneeq18@gmail.com>
2025-07-25 10:50:24 +0530

    Added a basic navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Logo from '@/assets/logo.svg';
 
 const Navbar : React.FC = () => {
     return <div id="Navbar">
         <div>
-
+            <img src="/logo.svg" alt="Logo" />
+            Quantum Computing @ IITI
+        </div>
+        <div>
+            <div>Home</div>
+            <div>Projects</div>
+            <div>Resources</div>
+            <div>Members</div>
         </div>
     </div>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,13 @@ import HeroSection from '@/components/content/HeroSection';
 import AboutSection from '@/components/content/AboutSection';
 import PathwaySection from '@/components/content/PathwaySection';
 import { Footer } from '@/components/layout/footer';
+import Navbar from '@/components/layout/navbar'
 
 const HomePage: React.FC = () => {
   return (
     <div className="homepage">
       <Layout>
+        <Navbar />
         <HeroSection />
         <AboutSection />
         <PathwaySection />
